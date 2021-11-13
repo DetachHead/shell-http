d7575311211227c6e30551f4bc9361f1b0c5b899
DetachHead <detachhead@users.noreply.github.com>
2021-11-13 19:37:08 +1000

    add stdin test

--- test.ts
+++ test.ts
@@ -6,11 +6,24 @@ import {
 } from "./deps.ts";
 import { createServer } from "./server.ts";
 
+const app = createServer(["git", "cat"]);
+
 Deno.test("no stdin", async () => {
-  const app = createServer(["git"]);
   const request = await superoak(app);
   await request.post("/git?args=status").expect((res: IResponse) => {
     assertEquals(res.status, 200);
     assertStringIncludes(res.body.stdout, "On branch");
   });
+});
+
+Deno.test({
+  name: "yes stdin",
+  ignore: Deno.build.os === "windows",
+  fn: async () => {
+    const request = await superoak(app);
+    await request.post("/cat").send("asdf").expect((res: IResponse) => {
+      assertEquals(res.status, 200);
+      assertStringIncludes(res.body.stdout, "asdf");
+    });
+  },
 });
